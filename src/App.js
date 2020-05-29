4b79f101342f501ed06598fa5c8bd42cfbfd3fcc
HeenaKhetia <heenajoshi931@gmail.com>
2020-05-29 18:17:20 +0530

    React-Material-Project

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Material Design Working!!<br></br>
+          <Button variant="contained">Default</Button>
+          <Button variant="contained" color="primary">
+            Primary
+          </Button>
+          <Button variant="contained" color="secondary">
+            Secondary
+          </Button>
+          <Button variant="contained" disabled>
+            Disabled
+          </Button>
+          <Button variant="contained" color="primary" href="#contained-buttons">
+            Link
+          </Button>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
